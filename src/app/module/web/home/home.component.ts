7a2edb450c35be586cbec86e1c7e504fc719a291
FelixMartinez <felixmartinez2503@gmail.com>
2020-07-08 13:53:57 -0500

    Estructura menu y add libreria owl-carousel

--- src/app/module/web/home/home.component.ts
+++ src/app/module/web/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
 import {FormBuilder} from "@angular/forms";
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +10,32 @@ import {FormBuilder} from "@angular/forms";
 })
 
 export class HomeComponent implements OnInit {
+
+  public customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['Previous', 'Next'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
   constructor(
     private _fb: FormBuilder
   ) {
--- src/app/module/web/web.module.ts
+++ src/app/module/web/web.module.ts
@@ -5,7 +5,7 @@ import { WebRoutes } from './web.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HomeComponent} from './home/home.component';
 import {Md5} from "ts-md5";
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [WebComponent, HomeComponent ],
@@ -13,10 +13,11 @@ import {Md5} from "ts-md5";
     WebRoutes,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CarouselModule
   ],
   providers: [
     Md5
-  ]
+  ],
 })
 export class WebModule { }
