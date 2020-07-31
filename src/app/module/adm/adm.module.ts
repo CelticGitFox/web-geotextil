a8b7d812abb87610d18c88cfa9877cd181a8153a
CelticGitFox <hardrockheroes19@gmail.com>
2020-07-31 16:40:53 -0500

    Responsive y maquetación de listado blog y Nosotros. Se añadieron imagenes para el footer de la sección nosotros.

--- src/app/module/adm/adm.module.ts
+++ src/app/module/adm/adm.module.ts
@@ -7,31 +7,32 @@ import {Md5} from 'ts-md5';
 import {ProfileComponent} from './profile/profile.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import {NotificationComponent} from './notification/notification.component';
-import {RequestCommerceComponent} from './requestCommerce/requestCommerce.component';
-import {MatTableModule} from '@angular/material/table';
-import { MatPaginatorModule} from '@angular/material/paginator';
-import {RequestJobsComponent} from './requestJobs/requestJobs.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {RequestPromotionsComponent} from './requestPromotions/requestPromotions.component';
-import {ListCommerceComponent} from './listCommerce/listCommerce.component';
-import {ListUsersComponent} from './listUsers/listUsers.component';
-import {ListJobsComponent} from './listJobs/listJobs.component';
-import {ListPromotionsComponent} from './listPromotions/listPromotions.component';
-import {DetailCommerceComponent} from './detailCommerce/detailCommerce.component';
-import {RegisterCommerceComponent} from './registerCommerce/registerCommerce.component';
-import {DetailuserComponent} from './detailuser/detailuser.component';
-import {DetailJobsComponent} from './detailJobs/detailJobs.component';
-import {DetailPromotionsComponent} from './detailPromotions/detailPromotions.component';
+import {NotificationComponent} from "./notification/notification.component";
+import {RequestCommerceComponent} from "./requestCommerce/requestCommerce.component";
+import {MatTableModule} from "@angular/material/table";
+import { MatPaginatorModule} from "@angular/material/paginator";
+import {RequestJobsComponent} from "./requestJobs/requestJobs.component";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {RequestPromotionsComponent} from "./requestPromotions/requestPromotions.component";
+import {ListCommerceComponent} from "./listCommerce/listCommerce.component";
+import {ListUsersComponent} from "./listUsers/listUsers.component";
+import {ListJobsComponent} from "./listJobs/listJobs.component";
+import {ListPromotionsComponent} from "./listPromotions/listPromotions.component";
+import {DetailCommerceComponent} from "./detailCommerce/detailCommerce.component";
+import {RegisterCommerceComponent} from "./registerCommerce/registerCommerce.component";
+import {DetailuserComponent} from "./detailuser/detailuser.component";
+import {DetailJobsComponent} from "./detailJobs/detailJobs.component";
+import {DetailPromotionsComponent} from "./detailPromotions/detailPromotions.component";
+import { ListBlogComponent } from './listBlog/listBlog.component';
+import { RegisterBlogComponent } from './registerBlog/registerBlog.component';
 
 
 @NgModule({
   declarations: [AdmComponent, ProfileComponent, NotificationComponent, RequestCommerceComponent, RequestJobsComponent, RequestPromotionsComponent, ListCommerceComponent, ListUsersComponent,
-  ListJobsComponent, ListPromotionsComponent, DetailCommerceComponent, RegisterCommerceComponent, DetailuserComponent, DetailJobsComponent, DetailPromotionsComponent, ListBlogComponent],
+  ListJobsComponent, ListPromotionsComponent, DetailCommerceComponent, RegisterCommerceComponent, DetailuserComponent, DetailJobsComponent, DetailPromotionsComponent,ListBlogComponent, RegisterBlogComponent],
   imports: [
     AdmRoutes,
     CommonModule,
